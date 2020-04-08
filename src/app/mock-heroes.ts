1cd6c330e2fc65c5c7be30aed63ca50dc13a0aaf
Zachary Reiss <zachary.reiss123@gmail.com>
2020-04-08 00:34:34 -0600

    Display list of heroes with css

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr. Really Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Maze'},
+  {id: 14, name: 'Bombasto'},
+  {id: 15, name: 'Mr. Cool'},
+  {id: 16, name: 'Rubberband man'},
+  {id: 17, name: 'Smarty pants'},
+  {id: 18, name: 'Magma'},
+  {id: 19, name: 'Storm'},
+  {id: 20, name: 'Dynama'}
+]
+
